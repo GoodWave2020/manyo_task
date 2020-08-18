b4975c95161e76032999b03ed3e99c7e9eb05f00
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-18 03:09:23 +0000

    add : Task新規作成画面を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,22 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice:"Taskが追加されました。"
+    else
+      render :new
+    end
+  end
+end
+
+private
+def task_params
+  params.require(:task).permit(:name, :content)
 end
