3c80b85018488370c09abebecb9b671af85cca35
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-18 02:48:35 +0000

    add : name, contentテーブルにDB側の制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_014619) do
+ActiveRecord::Schema.define(version: 2020_08_18_024243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.text "content"
+    t.string "name", null: false
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
