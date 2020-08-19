55b411954d129e67c9ddbd71528dbe7003f6020f
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-19 02:30:03 +0000

    add : タスク一覧が作成日時順に表示されるか検証するテストを作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task1) { FactoryBot.create(:task, name: 'タスク1') }
+  let!(:task2) { FactoryBot.create(:task, name: 'タスク2') }
+  before do
+    visit tasks_path
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -17,6 +22,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         task = FactoryBot.create(:task, name: 'task')
         visit tasks_path
         expect(page).to have_content 'task'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'タスク2'
       end
     end
   end
