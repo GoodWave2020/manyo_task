8b57b46fb922172da6cde672facebba7105fe8ba
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 13:30:46 +0000

    add : systemスペックにて検索とステータスのテストの追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :task do
     name { 'test_name' }
     content { 'test_content' }
-    dead_line { DateTime.now }
+    dead_line { '2020-07-01 00:00:00' }
+    status { '未着手' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task1) { FactoryBot.create(:task, dead_line: '2019-10-04 15:25:07') }
-  let!(:task2) { FactoryBot.create(:task, name: 'タスク1') }
+  let!(:task1) { FactoryBot.create(:task, dead_line: '2019-10-04 00:00:00') }
+  let!(:task2) { FactoryBot.create(:task, name: 'タスク1', status: '着手中') }
   let!(:task3) { FactoryBot.create(:task, name: 'タスク2') }
   before do
     visit tasks_path
@@ -13,7 +13,10 @@ RSpec.describe 'タスク管理機能', type: :system do
         fill_in :task_name, with: 'task'
         fill_in :task_content, with: 'task'
         select 2015, from: :task_dead_line_1i
+        select '完了', from: :task_status
         click_on "登録する"
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content '完了'
         expect(page).to have_content 'task'
         expect(page).to have_content 2015
       end
@@ -35,9 +38,27 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが終了期限の昇順に並んでいる場合' do
       it '終了期限が古いタスクが一番上に表示される' do
-        click_on "終了期限が近い順に並び替え"
+        click_on :sort_by_dead_line
         task_list = all('.task_row')
-        expect(task_list[0]).to have_content '2019-10-04 15:25:07'
+        expect(task_list[0]).to have_content '2019-10-04'
+      end
+    end
+    context 'タスクが検索された場合' do
+      it 'タイトルで検索できる' do
+        fill_in :name, with: '2'
+        click_on '検索'
+        expect(page).to have_content 'タスク2'
+      end
+      it 'ステータスで検索できる' do
+        select '着手中', from: :search
+        click_on '検索'
+        expect(page).to have_content 'タスク1'
+      end
+      it 'タイトルとステータスで検索できる' do
+        fill_in :name, with: '1'
+        select '着手中', from: :search
+        click_on '検索'
+        expect(page).to have_content 'タスク1'
       end
     end
   end
