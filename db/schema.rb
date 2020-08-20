02910d515a4d8df34a7440dffea3a295e63dbf8d
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 20:59:09 +0000

    add : priorityにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_204416) do
+ActiveRecord::Schema.define(version: 2020_08_20_205502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_204416) do
     t.datetime "updated_at", null: false
     t.datetime "dead_line", null: false
     t.string "status", default: "未着手", null: false
-    t.integer "priority", default: 0
+    t.integer "priority", default: 0, null: false
     t.index ["name"], name: "index_tasks_on_name"
   end
 
