05ced398f6e2450cbf7dd3e179238579b74e7020
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 21:05:34 +0000

    add : enumによりpriorityカラムを文字列化

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,9 @@ class Task < ApplicationRecord
   validates :status, presence: true
   scope :title_search, -> (query) { where('name like ?', "%#{query}%") }
   scope :status_search, -> (query) { where(status: query) }
+  enum priority: {
+    高: 0,
+    中: 1,
+    低: 2
+  }
 end
