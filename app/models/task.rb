4eec5bb69d9a01c65db1dcd29973378d3a916459
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 19:17:39 +0000

    add : statusのバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
   validates :dead_line, presence: true
+  validates :status, presence: true
 end
