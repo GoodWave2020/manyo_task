e6ad571085d4154767cfb74de3cf770bb7f8a634
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 19:43:01 +0000

    add : タイトルとステータスにモデルでscopeを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
   validates :content, presence: true
   validates :dead_line, presence: true
   validates :status, presence: true
+  scope :title_search, -> (query) { where('name like ?', "%#{query}%") }
+  scope :status_search, -> (query) { where(status: query) }
 end
