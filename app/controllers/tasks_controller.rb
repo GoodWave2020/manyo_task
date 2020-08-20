4c2a7daee1805e7c20887faccc2277ff7cabd059
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 02:49:42 +0000

    add : view側で終了期限の入力欄と一覧に表示する機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,7 +39,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :content)
+    params.require(:task).permit(:name, :content, :dead_line)
   end
 
   def set_task
