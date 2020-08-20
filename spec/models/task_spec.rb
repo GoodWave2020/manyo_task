29211ed940877fad2c2548ae1197820b907d5ef2
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-20 19:59:36 +0000

    add : モデルでの検索機能のテストを追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -15,8 +15,33 @@ describe 'タスクモデル機能', type: :model do
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        task = Task.new(name: '成功テスト', content: '成功テスト')
+        task = Task.new(name: '成功テスト', content: '成功テスト', dead_line: DateTime.now)
         expect(task).to be_valid
+      end
+    end
+  end
+  describe '検索機能' do
+    let!(:task) { FactoryBot.create(:task, name: 'task', status: '完了') }
+    let!(:second_task) { FactoryBot.create(:task, name: "sample") }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        expect(Task.title_search('task')).to include(task)
+        expect(Task.title_search('task')).not_to include(second_task)
+        expect(Task.title_search('task').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        expect(Task.status_search('完了')).to include(task)
+        expect(Task.status_search('完了')).not_to include(second_task)
+        expect(Task.status_search('完了').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        expect(Task.title_search('task').status_search('完了')).to include(task)
+        expect(Task.title_search('task').status_search('完了')).not_to include(second_task)
+        expect(Task.title_search('task').status_search('完了').count).to eq 1
       end
     end
   end
