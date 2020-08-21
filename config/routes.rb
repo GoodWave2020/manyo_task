bf696a65b1879b5cb70350e9270a7793ee6b34af
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-21 09:11:30 +0000

    add : セッション機能の雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   root 'tasks#index'
 end
