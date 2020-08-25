0d1648677e8f527d8fcac51946d848b1b65cb25c
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-25 09:06:36 +0000

    add : N+1問題の回避を取り入れ

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :current_user
   before_action :administrate_user
   def index
-    @users = User.all
+    @users = User.all.includes(:tasks)
   end
 
   def new
