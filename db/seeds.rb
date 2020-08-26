f9e8f4a497242e19f87341629dfddfcfe1edc3e3
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-26 07:44:30 +0000

    change : seedデータを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: 'first',
-               email: 'first@first.com',
+User.create!(name: 'second',
+               email: 'second@second.com',
                password: 'password',
                password_confirmation: 'password',
                admin: true
