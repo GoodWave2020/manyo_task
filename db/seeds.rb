b0c38e5a066dc14e8da1ef4cc4b718a002ad901a
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-27 09:28:01 +0000

    add : seed値を各モデル10個ずつ作れる機能を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: 'second',
-               email: 'second@second.com',
-               password: 'password',
-               password_confirmation: 'password',
-               admin: true
-               )
-Label.create!(name: 'デート')
+1.upto(10){|num|
+   User.find_or_initialize_by(id: num).update_attributes!(
+       name: "Mr.sample#{num}",
+       email: "number#{num}@seeds.com",
+       password: 'password',
+       password_confirmation: 'password',
+       admin: true
+    )
+}
+
+1.upto(10){|num|
+   Label.find_or_initialize_by(id: num).update_attributes!(
+       name: "number#{num}"
+    )
+}
+
+1.upto(10){|num|
+   Task.find_or_initialize_by(id: num).update_attributes!(
+       name: "task_number#{num}",
+       content: "task_content#{num}",
+       dead_line: '2020-07-01 00:00:00',
+       status: '未着手',
+       priority: 2,
+       user_id: 1
+    )
+}
