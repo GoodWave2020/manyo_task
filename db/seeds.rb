4f18fbcd6cb1f7383c9fe9a64c52a83c84cb8a88
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-28 07:09:21 +0000

    change : seedデータの挿入方法を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,22 @@
-1.upto(10){|num|
-   User.find_or_initialize_by(id: num).update_attributes!(
-       name: "Mr.sample#{num}",
-       email: "number#{num}@seeds.com",
-       password: 'password',
-       password_confirmation: 'password',
-       admin: true
-    )
-}
+10.times do |n|
+  User.create!(name: "Mr.sample#{n}",
+               email: "number#{n}@seeds.com",
+               password: 'password',
+               password_confirmation: 'password',
+               admin: true
+               )
+end
 
-1.upto(10){|num|
-   Label.find_or_initialize_by(id: num).update_attributes!(
-       name: "number#{num}"
-    )
-}
+10.times do |n|
+  Label.create!(name: "number#{n}")
+end
 
-1.upto(10){|num|
-   Task.find_or_initialize_by(id: num).update_attributes!(
-       name: "task_number#{num}",
-       content: "task_content#{num}",
-       dead_line: '2020-07-01 00:00:00',
-       status: '未着手',
-       priority: 2,
-       user_id: 1
-    )
-}
+10.times do |n|
+   Task.create!(name: "task_number#{n}",
+                content: "task_content#{n}",
+                dead_line: '2020-07-01 00:00:00',
+                status: '未着手',
+                priority: 2,
+                user_id: User.find_by(name: 'Mr.sample1').id
+                )
+end
